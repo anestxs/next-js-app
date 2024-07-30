c3a5960e867af07f0fe9cf2ac4195093dd5fc2a9
Zahar <crtrever@gmail.com>
2024-07-31 00:16:36 +0300

    Global errors and just errors  bounderies are added

---
+++ src/app/(admin)/companies/[...id]/page.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import Header from '../../../components/header';
+import { notFound } from 'next/navigation';
+
+export interface PageProps {
+  params: { id: string };
+}
+
+export default function Page({ params }: PageProps) {
+  useEffect(() => {
+    const id = Number.parseInt(params.id);
+    if (Number.isNaN(id)) {
+      notFound();
+    }
+  }, [params.id]);
+  return (
+    <>
+      <Header>Companies {params.id}</Header>
+      <p>{new Date().toTimeString()}</p>
+    </>
+  );
+}
